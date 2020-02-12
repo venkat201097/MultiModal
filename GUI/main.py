967567ccde872c2e1b8eb0ef5fc0bd4cbeefc8d8
Venkat Krishnamohan <venkat201097@gmail.com>
2020-02-12 16:15:50 +0530

    GUI v2

--- GUI/main.py
+++ GUI/main.py
@@ -3,6 +3,7 @@ import os
 import sys
 import pickle
 import pandas as pd
+import random
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static'
@@ -15,12 +16,16 @@ def traingenerator():
 
 def testgenerator():
 	global retrieve
+	i = 0
 	retrieve = 'i'
-	for sample in testaud[bucket]:
-		yield sample
+	for sample in random.sample(testaud[bucket],len(testaud[bucket])):
+		i+=1
+		yield sample,i
+	i = 0
 	retrieve = 'a'
-	for sample in testimg[bucket]:
-		yield sample
+	for sample in random.sample(testimg[bucket],len(testimg[bucket])):
+		i+=1
+		yield sample,i
 
 def storetest(form_dict):
 	global testsample
@@ -31,7 +36,7 @@ def storetest(form_dict):
 	else:
 		# response = form_dict['label']
 		response = list(form_dict)[0]
-		print(response,file=sys.stderr)
+		# print(response,file=sys.stderr)
 		sample = testsample[0].split('_')[0].split('/')[-1]
 	sampleid = testsample[0]
 	if retrieve=='i':
@@ -49,8 +54,8 @@ def storetest(form_dict):
 				labelid = i
 			if i.split('_')[1].split('/')[-1]==response:
 				responseid = i
-
-	with open('static/Data/subject_0.csv','a+') as fp:
+	global sub_id
+	with open('static/Data/S{}/test.csv'.format(sub_id),'a+') as fp:
 		fp.write('{},{},{},{},{},{},{},{}\n'.format(bucket,retrieve,sample,sampleid,label,labelid,response,responseid))
 
 @app.route('/')
@@ -63,22 +68,60 @@ def home():
 @app.route('/info',methods=['GET','POST'])
 def info():
 	with open('static/Data/subjects.csv','a+') as fp:
-		fp.write('{},{},{}\n'.format(request.form['name'],request.form['gender'],request.form['email']))
+		fp.write('{},{},{},{}\n'.format(request.form['id'],request.form['name'],request.form['gender'],request.form['email']))
+	global sub_id
+	sub_id = request.form['id']
+
+	if not os.path.isdir('static/Data/S'+request.form['id']):
+		os.system('mkdir '+'static/Data/S'+request.form['id'])
+
+	with open('static/Data/S{}/test.csv'.format(sub_id),'w+') as fp:
+		fp.write('Session,Test_Phase,Sample,Sample_Id,Label,Label_Id,Response,Response_Id\n')
+
+	with open('static/Data/S{}/train.csv'.format(sub_id),'w+') as fp:
+		fp.write('Session,Sample,Sample_Id,Label,Label_Id,PlayCount,Time\n')
+
 	global train_bucket
 	train_bucket = traingenerator()
 	return render_template('safety.html', session=bucket,sess_type="train")
 
+@app.route('/traininfo',methods=['GET','POST'])
+def traininfo():
+	global sub_id
+	playcount = request.form['playcount']
+	timespent = request.form['timespent']
+
+	with open('static/Data/S{}/train.csv'.format(sub_id),'a+') as fp:
+		fp.write('{},{}\n'.format(playcount,timespent))
+
+	return redirect(url_for("train"))
+
+
+
 @app.route('/train',methods=['GET','POST'])
 def train():
+	global sub_id
 	global bucket
 	global train_bucket
 	try:
 		trainsample = train_bucket.__next__()
+		with open('static/Data/S{}/train.csv'.format(sub_id),'a+') as fp:
+			sampleid = trainsample[0]
+			sample = trainsample[0].split('_')[0].split('/')[-1]
+			labelid = trainsample[1]
+			label = trainsample[1].split('_')[1].split('/')[-1]
+			fp.write('{},{},{},{},{},'.format(bucket,sample,sampleid,label,labelid))
 	except:
 		global test_bucket
 		test_bucket = testgenerator()
 		return render_template("safety.html", session=bucket,sess_type="test")
 	return render_template("train.html", image = trainsample[0],label=trainsample[1])
+
+@app.route('/score',methods=['GET','POST'])
+def score():
+	if bucket==NUM_BUCKETS:
+		return render_template('end.html')
+	return render_template('safety.html', session=bucket,sess_type="train")
 
 @app.route('/test',methods=['GET','POST'])
 def test():
@@ -88,23 +131,32 @@ def test():
 		storetest(request.form)
 	global testsample
 	try:
-		testsample = test_bucket.__next__()
+		testsample,curr_index = test_bucket.__next__()
 	except:
 		global bucket
 		bucket+=1
-		if bucket==NUM_BUCKETS:
-			return render_template('end.html')
-		else:
+		if bucket<NUM_BUCKETS:
 			global train_bucket
 			train_bucket = traingenerator()
-			return render_template('safety.html', session=bucket,sess_type="train")
+		global sub_id
+		responses = pd.read_csv('static/Data/S{}/test.csv'.format(sub_id))
+		curr_resp = responses[responses['Session']==(bucket-1)]
+		# print(bucket-1)
+		# print(curr_resp)
+		scores = {'i':0,'a':0}
+		for i,x in curr_resp.iterrows():
+			if x['Label']==x['Response']:
+				# print('yes')
+				scores[x['Test_Phase']]+=1
+		return render_template('score.html',session=bucket,sess_type="train",score_i=scores['i'],score_a=scores['a'])
+			# return render_template('safety.html', session=bucket,sess_type="train")
 
-	return render_template("test.html", samples= testsample[1:],label=testsample[0],retrieve=retrieve,proceed="hidden")
+	return render_template("test.html", samples= testsample[1:],label=testsample[0],retrieve=retrieve,index=curr_index)
 
 if __name__ == "__main__":
 	CLASSES_PER_BUCKET = 10
 	NUM_BUCKETS = 6
-	bucket = 5
+	bucket = 0
 	retrieve = 'i'
 
 	testsample=None
--- GUI/static/pickles/create.py
+++ GUI/static/pickles/create.py
@@ -17,6 +17,11 @@ for i in audios:
     audios[i] = ['static/audios/{}/{}'.format(j.split('/')[-2],j.split('/')[-1]) for j in audios[i]]
 
 train2 = []
+
+tempimg0 = []
+tempaud0 = []
+tempimg3 = []
+tempaud3 = []
 for x,k in enumerate([1,2,3,1,2,3]):
     temp = []
     for j in range(k):
@@ -27,11 +32,17 @@ for x,k in enumerate([1,2,3,1,2,3]):
             b = random.sample(audios[img2lab[i]],1)[0]
             del audios[img2lab[i]][audios[img2lab[i]].index(b)]
             temp.append((a,b))
+            if x==0:
+                tempimg0.append([a])
+                tempaud0.append([b])
+            if x==3:
+                tempimg3.append([a])
+                tempaud3.append([b])
     train2.append(temp)
 
 imgtest = []
 audtest = []
-for bucket in buckets:
+for z,bucket in enumerate(buckets):
     tempimg = []
     tempaud = []
     for i in bucket:
@@ -48,17 +59,53 @@ for bucket in buckets:
         tempaud.append([b,a])
     
     for x,i in enumerate(bucket):
-        for y,j in enumerate(random.sample(bucket[:x]+bucket[x+1:],4)):
+        for y,j in enumerate(random.sample(bucket[:x]+bucket[x+1:],9)):
             b = random.sample(audios[img2lab[j]],1)[0]
 #            del audios[img2lab[j]][audios[img2lab[j]].index(b)]
             tempimg[x].append(b)
         
-        for y,j in enumerate(random.sample(bucket[:x]+bucket[x+1:],4)):
+        for y,j in enumerate(random.sample(bucket[:x]+bucket[x+1:],9)):
             a = random.sample(images[j],1)[0]
 #            del images[j][images[j].index(a)]
             tempaud[x].append(a)
+        
         tempimg[x][1:] = random.sample(tempimg[x][1:],len(tempimg[x][1:]))
         tempaud[x][1:] = random.sample(tempaud[x][1:],len(tempaud[x][1:]))
+    
+    if z==0:
+        for x,i in enumerate(bucket):
+            for y,j in enumerate(random.sample(bucket[:x]+bucket[x:],10)):
+                b = random.sample(audios[img2lab[j]],1)[0]
+    #            del audios[img2lab[j]][audios[img2lab[j]].index(b)]
+                tempimg0[x].append(b)
+            
+            for y,j in enumerate(random.sample(bucket[:x]+bucket[x:],10)):
+                a = random.sample(images[j],1)[0]
+    #            del images[j][images[j].index(a)]
+                tempaud0[x].append(a)
+            
+            tempimg0[x][1:] = random.sample(tempimg[x][1:],len(tempimg0[x][1:]))
+            tempaud0[x][1:] = random.sample(tempaud[x][1:],len(tempaud0[x][1:]))
+            
+        tempimg += tempimg0
+        tempaud += tempaud0
+    
+    if z==3:
+        for x,i in enumerate(bucket):
+            for y,j in enumerate(random.sample(bucket[:x]+bucket[x:],10)):
+                b = random.sample(audios[img2lab[j]],1)[0]
+    #            del audios[img2lab[j]][audios[img2lab[j]].index(b)]
+                tempimg3[x].append(b)
+            
+            for y,j in enumerate(random.sample(bucket[:x]+bucket[x:],10)):
+                a = random.sample(images[j],1)[0]
+    #            del images[j][images[j].index(a)]
+                tempaud3[x].append(a)
+            
+            tempimg3[x][1:] = random.sample(tempimg[x][1:],len(tempimg3[x][1:]))
+            tempaud3[x][1:] = random.sample(tempaud[x][1:],len(tempaud3[x][1:]))
+        tempimg += tempimg3
+        tempaud += tempaud3
     
     imgtest.append([tuple(i) for i in tempimg])
     audtest.append([tuple(i) for i in tempaud])
